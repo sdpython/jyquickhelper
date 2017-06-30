8a27bc7a804382936dff71f4043ebe5a875889d5
xavier dupré <xavier.dupre@gmail.com>
2017-06-30 23:30:07 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -192,7 +192,7 @@ if is_local():
         fLOG=logging_function, covtoken=("24e81424-08ba-4ca7-be23-66c12926565f ", "'_UT_36_std' in outfile"))
     if not r and not ({"bdist_msi", "sdist",
                        "bdist_wheel", "publish", "publish_doc", "register",
-                       "upload_docs", "bdist_wininst"} & set(sys.argv)):
+                       "upload_docs", "bdist_wininst", "build_ext"} & set(sys.argv)):
         raise Exception("unable to interpret command line: " + str(sys.argv))
 else:
     r = False
