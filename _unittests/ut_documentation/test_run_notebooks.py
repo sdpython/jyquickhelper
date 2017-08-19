51f62924527f1270a77a203a8d71b820d6a39844
xavier dupré <xavier.dupre@gmail.com>
2017-08-19 20:57:52 +0200

    fix missing dependency

--- _unittests/ut_documentation/test_run_notebooks.py
+++ _unittests/ut_documentation/test_run_notebooks.py
@@ -39,7 +39,7 @@ except ImportError:
     import src
 
 from pyquickhelper.loghelper import fLOG
-from pyquickhelper.pycode import get_temp_folder
+from pyquickhelper.pycode import get_temp_folder, is_travis_or_appveyor
 from pyquickhelper.ipythonhelper import execute_notebook_list, execute_notebook_list_finalize_ut
 from pyquickhelper.ipythonhelper import install_python_kernel_for_unittest
 import src.jyquickhelper
