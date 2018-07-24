b24993b77650eada6197387eb798af145ce8f4f6
xavier dupré <xavier.dupre@gmail.com>
2018-07-24 23:47:12 +0200

    split unit test

--- _unittests/ut_documentation/test_run_notebooks_custom_js.py
+++ _unittests/ut_documentation/test_run_notebooks_custom_js.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-@brief      test log(time=10s)
+@brief      test log(time=11s)
 """
 
 import sys
@@ -26,7 +26,7 @@ except ImportError:
 import src.jyquickhelper
 
 
-class TestRunNotebooksPythonCustom(unittest.TestCase):
+class TestRunNotebooksPythonCustoJs(unittest.TestCase):
 
     @unittest.skipIf(sys.version_info[0] == 2, "notebook only working with python 3")
     def test_run_notebook(self):
@@ -35,14 +35,14 @@ class TestRunNotebooksPythonCustom(unittest.TestCase):
             self._testMethodName,
             OutputPrint=__name__ == "__main__")
 
-        temp = get_temp_folder(__file__, "temp_run_notebooks_custom")
+        temp = get_temp_folder(__file__, "temp_run_notebooks_custom_js")
 
         fnb = os.path.normpath(os.path.join(
             os.path.abspath(os.path.dirname(__file__)), "..", "..", "_doc", "notebooks"))
         keepnote = []
         for f in os.listdir(fnb):
             if os.path.splitext(f)[-1] == ".ipynb" and "_long" not in f and \
-                "js" not in f and \
+                "js" in f and \
                 ("custom" in f or "local" in f):
                 keepnote.append(os.path.join(fnb, f))
         for i, f in enumerate(keepnote):
