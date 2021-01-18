1eba7618b9b8b25babc3a448d66f7b0999633d44
xavier dupré <xavier.dupre@gmail.com>
2021-01-18 13:48:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -136,7 +136,7 @@ if is_local():
         layout=["html"], github_owner="sdpython",
         add_htmlhelp=sys.platform.startswith("win"),
         coverage_options=dict(omit=["*exclude*.py"]),
-        fLOG=logging_function, covtoken=("24e81424-08ba-4ca7-be23-66c12926565f ", "'_UT_37_std' in outfile"))
+        fLOG=logging_function, covtoken=("24e81424-08ba-4ca7-be23-66c12926565f ", "'_UT_39_std' in outfile"))
     if not r and not ({"bdist_msi", "sdist",
                        "bdist_wheel", "publish", "publish_doc", "register",
                        "upload_docs", "bdist_wininst", "build_ext"} & set(sys.argv)):
