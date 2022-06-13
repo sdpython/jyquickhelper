6824a16e32d1b7cc0cccf08d34cbbf54eb2f397d
xavier dupré <xavier.dupre@gmail.com>
2022-06-13 13:37:28 +0200

    Update test_code_style.py

--- _unittests/ut_module/test_code_style.py
+++ _unittests/ut_module/test_code_style.py
@@ -15,7 +15,8 @@ class TestCodeStyle(ExtTestCase):
         src_ = os.path.normpath(os.path.join(thi, "..", "..", "src"))
         check_pep8(src_, fLOG=fLOG,
                    pylint_ignore=('C0103', 'C1801', 'R1705', 'W0108', 'W0613',
-                                  'C0111', "W0212", 'C0415', 'C0209', 'W0107'),
+                                  'C0111', "W0212", 'C0415', 'C0209', 'W0107',
+                                  'C3001'),
                    skip=["Unable to import 'urllib2'",
                          "Redefining built-in 'format'",
                          "R1720",
@@ -26,7 +27,8 @@ class TestCodeStyle(ExtTestCase):
         test = os.path.normpath(os.path.join(thi, "..", ))
         check_pep8(test, fLOG=fLOG, neg_pattern="temp_.*",
                    pylint_ignore=('C0103', 'C1801', 'R1705', 'W0108', 'W0613',
-                                  'C0111', "W0212", 'C0415', 'C0209', 'W0107'),
+                                  'C0111', "W0212", 'C0415', 'C0209', 'W0107',
+                                  'C3001'),
                    skip=["Instance of 'RenderJsVis' has no '_ipython_display_'",
                          "Instance of 'RenderJsDot' has no '_ipython_display_'",
                          "R1720",
