18c42b0d3edacf084eae0f2431ea4e3279897900
xavier dupré <xavier.dupre@gmail.com>
2023-01-01 15:01:19 +0100

    documentation

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -9,7 +9,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
 local_template = os.path.join(os.path.abspath(
     os.path.dirname(__file__)), "phdoc_templates")
 
-set_sphinx_variables(__file__, "jyquickhelper", "Xavier Dupré", 2021,
+set_sphinx_variables(__file__, "jyquickhelper", "Xavier Dupré", 2023,
                      "alabaster", alabaster.get_path(),
                      locals(), extlinks=dict(issue=(
                          'https://github.com/sdpython/jyquickhelper/issues/%s',
